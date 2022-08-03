e17fac1c199b99ee20aa09799a13223bc26561ff
austek <ali.ustek@collibra.com>
2022-08-03 11:01:30 +0100

    Initial Structure

--- plugins/README.md
+++ plugins/README.md
@@ -7,3 +7,7 @@ Plugin to support comma-separated text payloads, written in Rust.
 ## Protobuf
 
 Plugin to support Protobuf messages, written in Kotlin.
+
+## Avro
+
+Plugin to support Avro messages, written in Java
---
+++ plugins/avro/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("spotless-conventions")
+}
+
+version = "0.0.0"
+
+val junitVersion = "5.9.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
